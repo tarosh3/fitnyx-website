c4ad308de1b860371c3b522af5185310b71d55d2
Tarosh <taroshdeveloper@gmail.com>
2026-03-25 10:30:23 +0530

    Fix basename trailing slash

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './styles/style.css'
 import App from './App.jsx'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
